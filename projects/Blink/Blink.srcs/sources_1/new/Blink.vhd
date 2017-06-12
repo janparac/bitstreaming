358de490b9f25b3301282f219259a49e582a78fa
sparac <stefano.paracchino@edu.unito.it>
2017-06-12 12:08:19 +0200

    Versioning all sources

---
+++ projects/Blink/Blink.srcs/sources_1/new/Blink.vhd
@@ -0,0 +1,24 @@
+
+-- Blink: simple VHDL LED-blinking code
+
+
+entity Blink is :
+
+   port (
+   
+      SW  : in  bit ;
+      LED : out bit
+   
+   ) ;
+
+
+end entity Blink ;
+
+
+architecture RTL of Blink is :
+
+begin
+
+   LED <= SW ;
+
+end architecture RTL ;
